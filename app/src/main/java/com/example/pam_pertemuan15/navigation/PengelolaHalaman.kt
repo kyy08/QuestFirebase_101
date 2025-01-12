53c44f1bd4c21465f5428a79bcf6a66e911d6e2b
kyy08 <kyyelzz1@gmail.com>
2025-01-12 18:47:53 +0700

    membuat Pengelola Halaman

---
+++ app/src/main/java/com/example/pam_pertemuan15/navigation/PengelolaHalaman.kt
@@ -0,0 +1,33 @@
+package com.example.pam_pertemuan15.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+
+@Composable
+fun PengelolaHalaman (
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost (
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(route = DestinasiHome.route) {
+            HomeScreen (
+                navigateToltemEntry = { navController.navigate(DestinasiInsert.route) }
+            )
+        }
+
+        composable(route = DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+    }
+}
