e7d2bbe9bfdd6cda619e446a427f260afbad7fd3
kyy08 <kyyelzz1@gmail.com>
2025-01-12 18:52:44 +0700

    membuat HomeViewModel

---
+++ app/src/main/java/com/example/pam_pertemuan15/ui/viewmodel/HomeViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.pam_pertemuan15.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_pertemuan15.model.Mahasiswa
+import com.example.pam_pertemuan15.repository.RepositoryMhs
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+
+sealed class HomeUiState {
+    data class Success(val data: List<Mahasiswa>) : HomeUiState()
+    data class Error(val e: Throwable) : HomeUiState()
+    object Loading : HomeUiState()
+}
+
+class HomeViewModel (
+    private val repoMhs: RepositoryMhs
+): ViewModel() {
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun deleteMhs (mhs: Mahasiswa) {
+        viewModelScope.launch {
+            try {
+                repoMhs.deleteMhs(mhs)
+            } catch (e: Exception) {
+                mhsUiState = HomeUiState.Error(e)
+            }
+        }
+    }
+
