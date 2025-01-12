61c23d8e196660c1b651a5863af02b5270a43677
kyy08 <kyyelzz1@gmail.com>
2025-01-12 23:22:16 +0700

    membuat InsertViewModel

---
+++ app/src/main/java/com/example/pam_pertemuan15/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,108 @@
+package com.example.pam_pertemuan15.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_pertemuan15.model.Mahasiswa
+import com.example.pam_pertemuan15.repository.RepositoryMhs
+import kotlinx.coroutines.launch
+
+data class MahasiswaEvent (
+    val nim: String = "",
+    val nama: String = "",
+    val gender: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun MahasiswaEvent.toMahasiswa(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    gender = gender,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+data class FormErrorState (
+    val nim: String? = null,
+    val nama: String? = null,
+    val gender: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && gender == null && alamat == null && kelas == null && angkatan == null
+    }
+}
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState()
+)
+
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
+
+class InsertViewModel (
+    private val mhs: RepositoryMhs
+): ViewModel() {
+
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // Memperbarui State berdasarkan input
+    fun updateUiEvent(event: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(insertUiEvent = event)
+    }
+
+    // Validasi data input pengguna
+    fun validateFields (): Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isEmpty()) "NIM tidak boleh kosong" else null,
+            nama = if (event.nama.isEmpty()) "Nama tidak boleh kosong" else null,
+            gender = if (event.gender.isEmpty()) "Jenis Kelamin tidak boleh kosong" else null,
+            alamat = if (event.alamat.isEmpty()) "Alamat tidak boleh kosong" else null,
+            kelas = if (event.kelas.isEmpty()) "Kelas tidak boleh kosong" else null,
+            angkatan = if (event.angkatan.isEmpty()) "Angkatan tidak boleh kosong" else null
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun insertMhs () {
+        if (validateFields()) {
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    mhs.insertMhs(uiEvent.insertUiEvent.toMahasiswa())
+                    uiState = FormState.Success("Berhasil Menambahkan Mahasiswa")
+                } catch (e: Exception) {
+                    uiState = FormState.Error("Gagal Menambahkan Mahasiswa")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data Tidak Valid")
+        }
+    }
+
+    fun resetForm() {
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
+
+    fun resetSnackBarMessage() {
+        uiState = FormState.Idle
+    }
+}
